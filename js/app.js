af799af6c6968f76784991d2941799a2cb35cea3
Vicktor M <vicktor.moberg@gmail.com>
2020-07-31 15:43:23 -0400

    added Character class, Player class, Dragon class, player, and dragon

--- js/app.js
+++ js/app.js
@@ -1,11 +1,57 @@
 console.log($)
 //////////////////Constants and Objects///////
-
+// const input = 
 ///Character Class///
-class Character = 
+class Character {
+    constructor (name= "", health= 0, strength= 0, dexterity=0, items= []){
+        this.name= name;
+        this.health = health;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.items = items;
+    }
+}
 ///Player class///
+class Player extends Character {
+    constructor (name=`${input.val()}`, health = 20, strength = 5, dexterity = 0.7, items =[]){
+        super(name, health, strength, dexterity, items)
+        // this.name= name;
+        // this.health = health;
+        // this.strength = strength;
+        // this.dexterity = dexterity;
+        // this.items = items;
+    }
+    attackDragon(Dragon){
+        if (Math.random() <= dragon.dexterity) {
+            displayPlayerAttack()
+             dragon.health= dragon.health - this.strength;
+        }   else displayMissedAttack()
+    return dragon.health
+    }
+    
+}
 ///Dragon class///
+class Dragon extends Character {
+    constructor (name=`${input.val()}`, health = 20, strength = 5, dexterity = 0.6, items =[]){
+        super(name, health, strength, dexterity, items)
+        // this.name= name;
+        // this.health = health;
+        // this.strength = strength;
+        // this.dexterity = dexterity;
+        // this.items = items;
+    }
+    attackPlayer(Player){
+        if (Math.random() <= player.dexterity) {
+            displayDragonAttack()
+             player.health= player.health - this.strength;
+        }   else displayMissedAttack()
+    return player.health
 
+    }
+}
+
+const player = new Player (`${input.val}`)
+const dragon = new Dragon ("Kh'o Vah", items['gold coins'])
 ////Divs to be appended///
 ///Text boxes///
 ///Buttons///
