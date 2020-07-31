4b89e0d1445e2474a0f356c9bede0ce8e2d3c718
Vicktor M <vicktor.moberg@gmail.com>
2020-07-31 14:52:32 -0400

    Files created. Psedo-code written in app.js.

---
+++ js/app.js
@@ -0,0 +1,34 @@
+console.log($)
+//////////////////Constants and Objects///////
+
+///Character Class///
+///Player class///
+///Dragon class///
+
+////Divs to be appended///
+///Text boxes///
+///Buttons///
+
+/////////////////////////Functions/////////////
+
+/////Display the instructions////
+
+/////take Player name///////
+
+///////Insert player name to Player character////
+
+///////Set the scene/////
+
+////////Choice to go left, right, or straight////
+
+//////Go left, pick up beer///////
+//////go back///
+
+//////Go straight, pick up sword///
+/////go back///
+
+
+///////Go Right, fight dragon/////
+
+//////Dragon fight///////
+
